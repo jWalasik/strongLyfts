a0bc296e9c35dd5267e718a5d230e85227b4c409
Jacek Walasik <jacekwalasik89@gmail.com>
2019-05-30 20:35:00 +0200

    moved index to root

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = (env)=>{
   return {
     entry: ['babel-polyfill', './src/app.js'],
     output: {
-      path: path.join(__dirname, 'public', 'dist'),
+      path: path.join(__dirname, 'build'),
       filename: 'bundle.js'
     },
     module: {
@@ -59,7 +59,7 @@ module.exports = (env)=>{
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
-      publicPath: '/dist/'
+      publicPath: ''
     }
   }
 }
