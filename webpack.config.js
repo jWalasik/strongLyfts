b7e8c06c4d041a0e32f96bd97f0a1e39cccc5c8a
elPaleniozord <jacekwalasik89@gmail.com>
2021-09-27 16:50:12 +0200

    fix netlify deploy

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   entry: ['./src/app.js'],
   output: {
-    path: path.resolve(__dirname, '/public/dist/'),
+    path: path.join(__dirname, 'public', 'dist'),
     filename: 'index.bundle.js'
   },
   devServer: {
