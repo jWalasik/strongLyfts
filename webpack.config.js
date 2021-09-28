2b22fcdc2a928d1a1b6d2be87e8008e3b1e89763
elPaleniozord <jacekwalasik89@gmail.com>
2021-09-28 13:44:38 +0200

    fix env setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,58 @@
 const path = require('path')
+const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  entry: ['./src/app.js'],
-  output: {
-    path: path.join(__dirname, 'public', 'dist'),
-    filename: 'bundle.js'
-  },
-  devServer: {
-    port: 3010
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+if(process.env.NODE_ENV === 'test') {
+  require('dotenv').config({path: '.env.test'})
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({path: '.env.development'})
+}
+
+module.exports = (env, args) => {
+  const isProduction = env === 'production'
+
+  return {
+    entry: ['./src/app.js'],
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
+    },
+    devServer: {
+      port: 8080
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            // MiniCssExtractPlugin.loader,
+            'css-loader?url=false',
+            'sass-loader'
+          ]
         }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          // MiniCssExtractPlugin.loader,
-          'css-loader?url=false',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  // plugins: [
-  //   new MiniCssExtractPlugin()
-  // ]
+      ]
+    },
+    plugins: [
+      // new MiniCssExtractPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+      })
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map'
+  }
 }
